241e64334de4b39b04cad1e6882ff03169fd5d2c
Said Hussein <saidhussein12342@gmail.com>
2022-08-16 18:23:57 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+    
+    def name=(name)
+        @name = name
+    end
 
+    def name 
+        @name 
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 end
+
+rex = Dog.new
+rex.name = "Rex"
+rex.breed = "Pitbull"
+
