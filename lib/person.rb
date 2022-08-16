b2c72a702473f7e4bada5dccb3382cfc7c2a509f
Said Hussein <saidhussein12342@gmail.com>
2022-08-16 18:23:50 +0300

    commit tree 893ee5d5414b438ea871f74af5aaede6f7213aab

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+    def name=(name)
+        @name = name
+    end
+
+    def name 
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job 
+        @job
+    end
+
 end
+
+Ahmed = Person.new
+Ahmed.name = "Ahmed"
+Ahmed.job = "Doctor"
